3a3c0bd2d65077a42d532e31baefe71299085cce
Elizaveta <lizayakovleva2005@gmail.com>
2026-02-28 12:46:11 +0200

    chore: complete foundation setup with Electron, React and Tailwind v4

--- src/renderer/src/components/Versions.vue
+++ src/renderer/src/components/Versions.vue
@@ -5,6 +5,9 @@ const versions = reactive({ ...window.electron.process.versions })
 </script>
 
 <template>
+<h1 className="text-3xl font-bold text-blue-500 underline">
+  Tailwind працює!
+</h1>
   <ul class="versions">
     <li class="electron-version">Electron v{{ versions.electron }}</li>
     <li class="chrome-version">Chromium v{{ versions.chrome }}</li>
